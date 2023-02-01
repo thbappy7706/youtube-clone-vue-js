ddb895e7d8604dc1584ed01b2731220aa13fd237
thbappy7706 <tanvir15-7706@diu.edu.bd>
2023-02-01 13:16:02 +0600

    video card ip

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,18 +1,29 @@
 <script setup>
 import { Head, Link } from '@inertiajs/vue3';
 import NavLayout from "@/Layouts/NavLayout.vue";
+import VideoCard from "@/Components/VideoCard.vue";
 
-defineProps({
-    canLogin: Boolean,
-    canRegister: Boolean,
-    laravelVersion: String,
-    phpVersion: String,
-});
+// defineProps({
+//     canLogin: Boolean,
+//     canRegister: Boolean,
+//     laravelVersion: String,
+//     phpVersion: String,
+// });
 </script>
 
 <template>
     <Head title="Youtube" />
 <NavLayout>
+    <div class="grid 2xl:grid-cols-5 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-2">
+        <VideoCard
+            title="Hello Title"
+            user="Hello User"
+            views="23k"
+            image="https://picsum.photos/id/230/100"
+            videoUrl="/Videos/Lake.mp4"
+            thumbnail="/Videos/Thumbnails/Lake.png"
+        />
+    </div>
 
 </NavLayout>
 
